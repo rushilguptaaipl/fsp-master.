5200ef789ec2341a6a4ab83d58aacf63c9cf6408
test <test@example.com>
2023-07-03 16:09:02 +0530

    added jwt Authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,37 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { SignInDto } from './dto/signin.dto';
 import { UsersService } from 'src/users/users.service';
-import { SignUpDto } from './dto/signup.dto';
+import { SignUpDto } from './dto/signup.dto'; 
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
   async signIn(signInDto: SignInDto): Promise<any> {
     const user = await this.usersService.findOne(signInDto.username);
     if (user?.password !== signInDto.password) {
-      throw new ConflictException();
+      throw new NotFoundException('user doest not exist');
     }
-    const { password, ...result } = user;
-    return result;
+
+    const payload = { sub: user.id,id: user.id ,username: user.username, roles: ['admin'] };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 
   async signUp(signUpDto: SignUpDto): Promise<any> {
     const user = await this.usersService.findOne(signUpDto.username);
     if (user) {
-      throw new ConflictException();
+      throw new ConflictException('user already exist');
     }
-   
+
     return await this.usersService.createUser(signUpDto);
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('users')
+@UseGuards(AuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  
+  @Get()
+  findAll(){
+    return this.usersService.findAll()
+  }
 }
